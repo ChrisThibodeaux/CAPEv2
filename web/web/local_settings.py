17bcf7a79a008e85d6a783f0ff9a9b6ec372003e
doomedraven <doommedraven@gmail.com>
2025-02-18 08:40:24 +0100

    unify abusech config
    Updates
    
    add

--- web/web/local_settings.py
+++ web/web/local_settings.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2010-2015 Cuckoo Foundation.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
-
+import os
 LOCAL_SETTINGS = True
 from .settings import *  # noqa: F403
 
